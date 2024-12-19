f52bc7ef74504c307336eb28cb20052577c5d611
kenshidoko <117933685+kenshidoko@users.noreply.github.com>
2024-12-20 01:02:18 +0800

    Delete script.lua

--- script.lua
+++
@@ -1,6 +0,0 @@
-getgenv().greenNumber = 72.68 -- keep this number around 72
-getgenv().unlockfps = true -- reccomend
-getgenv().keytoclick = "E" -- dont change this
-getgenv().customautogreen = Enum.KeyCode.R -- press R if you dont want to hold shoot button
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/kenshidoko/BBLegendsScript/refs/heads/main/KeySystem.lua'))()
